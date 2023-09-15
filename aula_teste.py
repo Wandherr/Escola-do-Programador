d700e6956b90193a556be10253db8a3290189ffd
Vanderley <122535781+Wandherr@users.noreply.github.com>
2023-09-14 21:23:05 -0300

    Create aula_teste.py

---
+++ aula_teste.py
@@ -0,0 +1,9 @@
+print('Olá digite seu nome.')
+
+nome = input('Digite o seu nome : ')
+idade = input('Digite sua idade : ')
+altura = input('Digite sua altaura : ')
+
+print('Seu nome é :', nome )
+print('Sua idade é :', idade)
+print('Sua altura é :', altura)
